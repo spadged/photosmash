c3107d8f3d58d2b1d3b8b8daf37e4388f97f4e05
Grant Meek <grant.meek@gmail.com>
2017-08-27 23:48:35 +0100

    - removed Q - replaced q with built in promises - added async / await

--- smash.js
+++ smash.js
@@ -1,10 +1,9 @@
-var fs = require('fs');
-var gm = require('gm');
-var im = gm.subClass({ imageMagick: true });
-var FFmpeg = require('fluent-ffmpeg');
-var prompt = require('prompt');
-var ExifImage = require('exif').ExifImage;
-var Q = require('q');
+const fs = require('fs');
+const gm = require('gm');
+const im = gm.subClass({ imageMagick: true });
+const FFmpeg = require('fluent-ffmpeg');
+const prompt = require('prompt');
+const ExifImage = require('exif').ExifImage;
 
 var dir = {
 	input: "./in/",
@@ -27,87 +26,288 @@ var FPS = {
 	out: 30
 }
 
-function smash()
+async function smash()
 {
-	Q.fcall(start)
-		.then(clearDirTemp)
-		.then(orderImages)
-		.then(proccessImages)
-		.then(buildVideo)
-		.catch(function(message)
-		{
-			console.log("Error > ", message);
-		})
-		.progress(function(message)
-		{
-			console.log(message);
-		})
-		.done(function()
-		{
-			console.log("Done");
-		});
-}
+	try
+	{
+		await start();
+		await clearDirTemp();
+		await orderImages();
+		await proccessImages();
+		//await buildVideo();
+		
+		console.log("Done");
+	}
+	catch(error)
+	{
+		console.log("Error > ", error)
+	}
 
-function genFileName()
-{
-	var now = new Date();
-
-	var sb = [
-		now.getFullYear(),
-		now.getMonth(),
-		now.getDate(),
-		now.getHours(),
-		now.getMinutes()
-	];
-
-	return  sb.join("-") + ".mp4";
 }
 
 function start()
 {
-	var errors = [];
-	
-	var deferred = Q.defer();
-	  
-	checkFolders();
-	
-	if(!hasInputImages())
+	return new Promise(function(resolve, reject) 
 	{
-		errors.push("The input folder does not have any valid images to process. Please add .jpg images.");
-	}
+		let errors = [];
+
+		checkFolders();
 	
-	if(errors.length == 0)
-	{
-		prompt.start();
+		if(!hasInputImages())
+		{
+			errors.push("The input folder does not have any valid images to process. Please add .jpg images.");
+		}
 		
-		prompt.get(['BPM'], function (err, result)
-		{			
-			FPS.in = bpmToIps(result.BPM);
+		if(errors.length == 0)
+		{
+			prompt.start();
 			
-			console.info("Processing > BPM: " + result.BPM + " | FPS: " +  FPS.out + " | IPS: " + FPS.in);
-	
-			list.files = getImageFileList();
-			
-			deferred.resolve();
-		});
-	}
-	else
-	{		
-        deferred.reject(errors.join("\n"));
-	}
-	
-	return deferred.promise;
+			prompt.get(['BPM'], function (err, result)
+			{			
+				FPS.in = bpmToIps(result.BPM);
+				
+				console.info("Processing > BPM: " + result.BPM + " | FPS: " +  FPS.out + " | IPS: " + FPS.in);
+		
+				list.files = getImageFileList();
+				
+				resolve();
+			});
+		}
+		else
+		{		
+			reject(errors.join("\n"));
+		}
+	});
 }
+
+function clearDirTemp()
+{
+	return new Promise(function(resolve, reject)
+	{
+		console.log("Clearing out temp files");
+
+		fs.readdir(
+			dir.temp,
+			function(err, files)
+			{
+				if (err)
+				{
+					console.log(err);
+
+					reject("Error clearing out temp dir");
+				}
+				else
+				{
+					if (files.length === 0)
+					{
+						resolve();
+					}
+					else
+					{
+						for(var i = 0; i < files.length; i++)
+						{
+							let fileName = files[i];
+
+							fs.unlinkSync(dir.temp + fileName);
+						}
+
+						resolve();
+					}
+				}
+			}
+		);
+	});
+}
+
+async function orderImages()
+{
+	return new Promise(async function(resolve, reject)
+	{
+		console.log('Getting EXIF data');
+
+		for(let i = 0; i < list.files.length; i++)
+		{
+			let name = list.files[i];
+
+			if(name != '.DS_Store')
+			{
+				await orderImage(name).catch(function(message)
+				{
+					console.log(message);
+				});
+			}
+		}
+
+		resolve();
+	});
+}
+
+function orderImage(path)
+{
+	return new Promise(function(resolve, reject)
+	{
+		console.log("Get EXIF >", path);
+
+		try
+		{
+			new ExifImage({ image : dir.input + path}, function (error, exifData)
+			{
+				if (error)
+				{
+					reject(error.message, ": ", path);
+				}
+				else
+				{
+					list.images.push({
+						"name": path,
+						"date": Number(getImageDate(exifData))
+					});
+					
+					console.log("Got EXIF >", path);
+
+					resolve();
+				}
+			});
+		}
+		catch (error)
+		{				
+			reject(error.message, ": ", path);	
+		}
+	});
+}
+
+async function proccessImages()
+{
+	return new Promise(async function(resolve, reject)
+	{
+		console.log("Processing images...");
+		
+		list.images.sort(sortImages);
+	
+		for(let i = 0; i < list.images.length; i++)
+		{
+			await processImage(list.images[i], i);
+		}
+
+		resolve();
+	});
+}
+
+function processImage(image, index)
+{
+	return new Promise(function(resolve, reject)
+	{
+		let width = size.width;
+	
+		let height = size.height;
+		
+		let imageNumber;
+		
+		switch(String(index).length)
+		{
+			case 1:
+				imageNumber = '000' + index;
+				break;
+			case 2:
+				imageNumber = '00' + index;
+				break;
+			case 3:
+				imageNumber = '0' + index;
+				break;
+			default:
+				imageNumber = index;
+				break;
+		}
+		
+		let imageName = "frame_" + imageNumber + ".png";
+
+		console.log("Processing frame: " + image.name + " >>> " + imageName);
+
+		try
+		{
+			im(dir.input + image.name)
+				.background('#000000')
+				.autoOrient()
+				.resize(width)
+				.gravity('Center')
+				.crop(width, height, 0, 0)
+				//.extent(width, height)
+				.noProfile()
+				.write(dir.temp + imageName, function (err)
+				{
+					if (!err)
+					{				
+						resolve('Finished processing image: ' + imageName);
+					}
+					else
+					{								
+						reject('Error processing images: ' + err);
+					}
+				});
+		}
+		catch(e)
+		{
+			reject(e);
+			console.log("Dropped frame!");
+		}
+	});
+}
+
+function buildVideo()
+{
+	return new Promise(function(resolve, reject)
+	{
+		console.log("Generating Video...");
+	
+		/*
+			Options 
+			- '-pix_fmt yuv420p' http://superuser.com/questions/704744/video-produced-from-bmp-files-only-plays-in-vlc-but-no-other-players
+		*/
+
+		let outputName = genFileName();
+
+		new FFmpeg({source: dir.temp + 'frame_%04d.png' })
+			.withNoAudio()
+			.withVideoCodec('libx264')
+			.withSize(size.width + 'x' + size.height)
+			.withVideoBitrate('2000k')
+			.withFpsInput(FPS.in)
+			.withFpsOutput(FPS.out)
+			.addOptions(['-crf 19', '-preset slow', '-pix_fmt yuv420p'])
+			.on('progress', function(progress)
+			{
+				//deferred.notify("Processing frames: " + progress.frames);
+				console.log("Processing frames: " + progress.frames);
+			})
+			.on('error', function(err, stdout, stderr)
+			{
+				var sb = [];
+				
+				sb.push('Cannot process video: ' + err.message);
+				sb.push("ffmpeg stdout:\n" + stdout);
+				sb.push("ffmpeg stderr:\n" + stderr);
+				
+				reject(sb.join("\n"));
+			})
+			.on('end', function()
+			{
+				resolve();
+			})
+			.saveToFile(dir.output + outputName);
+	});
+}
+
+/* utility mehods */
 
 function getImageFileList()
 {
-	var files = fs.readdirSync(dir.input);
+	let files = fs.readdirSync(dir.input);
 
-	var result = [];
+	let result = [];
 
 	for(var i = 0; i < files.length; i++)
 	{
-		var item = files[i];
+		let item = files[i];
 
 		if(item.toLowerCase().indexOf(".jpg") > -1)
 		{
@@ -120,21 +320,21 @@ function getImageFileList()
 
 function bpmToIps(bpm)
 {
-	var ipsRaw = (FPS.out * 60) / bpm;
+	let ipsRaw = (FPS.out * 60) / bpm;
 	
 	return Math.round(ipsRaw * 100) / 100;
 }
 
 function hasInputImages()
 {	
-	var entries = getImageFileList();
+	let entries = getImageFileList();
 	
 	return (entries.length > 0);
 }
 
 function checkFolders()
 {
-	for(var key in dir)
+	for(let key in dir)
 	{
 		if(!fs.existsSync(dir[key]))
 		{
@@ -143,101 +343,24 @@ function checkFolders()
 	}
 }
 
-function clearDirTemp()
+function genFileName()
 {
-	var deferred = Q.defer();
-	
-	console.log("Clearing out temp files");
+	let now = new Date();
 
-	fs.readdir(
-		dir.temp,
-		function(err, files)
-		{
-    		if (err)
-			{
-      			console.log(err);
+	let sb = [
+		now.getFullYear(),
+		now.getMonth(),
+		now.getDate(),
+		now.getHours(),
+		now.getMinutes()
+	];
 
-				deferred.reject("Error clearing out temp dir");
-    		}
-			else
-			{
-				if (files.length === 0)
-				{
-					deferred.resolve();
-				}
-				else
-				{
-					for(var i = 0; i < files.length; i++)
-					{
-						var fileName = files[i];
-
-						fs.unlinkSync(dir.temp + fileName);
-					}
-
-					deferred.resolve();
-				}
-			}
-		}
-	);
-
-	return deferred.promise;
-}
-
-function orderImages()
-{
-	var promises = [];
-	
-	console.log('Getting EXIF data');
-
-	for(var i = 0; i < list.files.length; i++)
-	{
-		var name = list.files[i];
-
-		if(name != '.DS_Store')
-		{
-			promises.push(orderImage(name));
-		}
-	}
-	
-	return Q.all(promises);
-}
-
-function orderImage(path)
-{
-	var deferred = Q.defer();
-
-	console.log("Getting EXIF data for: ", path);
-
-	try
-	{
-		new ExifImage({ image : dir.input + path}, function (error, exifData)
-		{
-			if (error)
-			{
-        		deferred.reject(error.message);
-			}
-			else
-			{
-				list.images.push({
-					"name": path,
-					"date": Number(getImageDate(exifData))
-				});
-				
-				deferred.resolve();
-			}
-		});
-	}
-	catch (error)
-	{				
-        deferred.reject(error.message);	
-	}
-	
-	return deferred.promise;
+	return sb.join("-") + ".mp4";
 }
 
 function getImageDate(data)
 {
-	var fileDate = "";
+	let fileDate = "";
 	
 	if(data.exif.hasOwnProperty("CreateDate"))
 	{
@@ -271,131 +394,8 @@ function sortImages(a,b)
 	}
 	else
 	{
-		 return 0;
+		return 0;
 	} 
-}
-
-function proccessImages()
-{
-	var promises = [];
-	
-	console.log("Processing images...");
-
-	list.images.sort(sortImages);
-	
-	for(var i = 0; i < list.images.length; i++)
-	{
-		promises.push(processImage(list.images[i], i));
-	}
-	
-	return Q.all(promises);
-}
-
-function processImage(image, index)
-{
-	var deferred = Q.defer();
-	
-	var width = size.width;
-	
-	var height = size.height;
-	
-	var imageNumber;
-	
-	switch(String(index).length)
-	{
-		case 1:
-			imageNumber = '000' + index;
-			break;
-		case 2:
-			imageNumber = '00' + index;
-			break;
-		case 3:
-			imageNumber = '0' + index;
-			break;
-		default:
-			imageNumber = index;
-			break;
-	}
-	
-	var imageName = "frame_" + imageNumber + ".png";
-
-	console.log("Processing frame: " + image.name + " >>> " + imageName);
-
-	try
-	{
-		im(dir.input + image.name)
-			.background('#000000')
-			.autoOrient()
-			.resize(width)
-			.gravity('Center')
-			.crop(width, height, 0, 0)
-			//.extent(width, height)
-			.noProfile()
-			.write(dir.temp + imageName, function (err)
-			{
-				if (!err)
-				{				
-					deferred.resolve('Finished processing image: ' + imageName);
-				}
-				else
-				{								
-					deferred.reject('Error processing images: ' + err);
-				}
-			});
-	}
-	catch(e)
-	{
-		deferred.reject('ERROR: ' + e);
-		console.log("Dropped frame!");
-	}
-		
-	return deferred.promise;
-}
-
-function buildVideo()
-{
-	console.log("Generating Video...");
-	
-	var deferred = Q.defer();
-	
-	/*
-		Options 
-		- '-pix_fmt yuv420p' http://superuser.com/questions/704744/video-produced-from-bmp-files-only-plays-in-vlc-but-no-other-players
-	*/
-
-	var outputName = genFileName();
-
-	new FFmpeg({source: dir.temp + 'frame_%04d.png' })
-		.withNoAudio()
-		.withVideoCodec('libx264')
-		.withSize(size.width + 'x' + size.height)
-		.withVideoBitrate('2000k')
-		.withFpsInput(FPS.in)
-		.withFpsOutput(FPS.out)
-		.addOptions(['-crf 19', '-preset slow', '-pix_fmt yuv420p'])
-		.on('progress', function(progress)
-		{
-			deferred.notify("Processing frames: " + progress.frames);
-		})
-		.on('error', function(err, stdout, stderr)
-		{
-			var sb = [];
-			
-			sb.push('Cannot process video: ' + err.message);
-			sb.push("ffmpeg stdout:\n" + stdout);
-			sb.push("ffmpeg stderr:\n" + stderr);
-			
-			deferred.reject(sb.join("\n"));
-		})
-		.on('end', function()
-		{
-			deferred.resolve();
-		})
-		.saveToFile(dir.output + outputName);
-
-		//todo: timestamp output filename
-	
-	return deferred.promise;
 }
 
 smash();
